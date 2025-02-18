dc60164412226f2b81ddadf49d6526b98e7dc6b8
Vlad Kurakin <kurakinvlad19@gmail.com>
2025-02-18 19:11:48 +0300

    Edit homework_L5_P2

--- lesson_5/Vladislav_home_work_L5_P2.py
+++ lesson_5/Vladislav_home_work_L5_P2.py
@@ -7,7 +7,7 @@ print("""
 #    введя вес(кг) и рост(м)
 #
 """)
-a = 0
+
 # Function for find BMI user.
 def BMI(w, h):
     """
@@ -17,16 +17,25 @@ def BMI(w, h):
     Выходные переменные:
         None или значение ИМТ.
     """
-    global a
     if w <= 0 or h <= 0:
-        return "Введите верные значения"
-    b = result = w / h ** 2
-    if result > 18.4:
-        a += 5
+        print("Введите верные значения")
+    result = w / h ** 2
+    if result < 18.5:
+        print(f"{result}\nУ вас дефицит массы тела. Попробуйте проследить за калориями.")
+    elif result < 25.1:
+        print(f"{result}\nУ вас норма массы тела. Продолжайте в том же духе.")
+    elif result < 30.1:
+        print(f"{result}\nУ вас предожирение массы тела. Попробуйте проследить за калориями.")
+    elif result < 35.1:
+        print(f"{result}\nУ вас ожирение | степени. Попробуйте проследить за калориями.")
+    elif result < 40.1:
+        print(f"{result}\nУ вас ожирение || степени. Попробуйте проследить за калориями.")
+    elif result < 45.1:
+        print(f"{result}\nУ вас ожирение ||| степени. Попробуйте обратиться к врачу.")
 
 
 # Input user characteristics.
 user_characteristics = input("Введите свой вес(кг) и рост(в метрах) через пробел: ")
 
 # Output BMI user.
-print(BMI(float(user_characteristics.split()[0]),float(user_characteristics.split()[1])))
+BMI(float(user_characteristics.split()[0]),float(user_characteristics.split()[1]))
