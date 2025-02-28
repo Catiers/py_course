7ada0e7d9e05aeb52342201ea7002f5f90a04788
Vlad Kurakin <kurakinvlad19@gmail.com>
2025-02-28 20:53:41 +0300

    Edit some files

--- lesson_11/queue.py
+++ lesson_11/queue.py
@@ -19,12 +19,51 @@ except EmptyQueueError as e:
 
     
 EmptyQueueError: message: Очередь пустая
->>> class Queue:
-...     def __init__(self):
-...         self.__queue = []
-...     def get(self)"
-...     
+class Queue:
+    def __init__(self):
+        self.__queue = []
+    def get(self)"
+    
 SyntaxError: unterminated string literal (detected at line 4)
+class Queue:
+    def __init__(self):
+        self.__queue = []
+    def get(self):
+        if len(self.__queue) < 1:
+            raise EmptyQueueError("Очередь пустая")
+        return self.__queue.pop(0)
+        
+    def put(self, value):
+        self.__queue.append(value)
+
+        
+q1 = Queue()
+q1.put(1)
+q1.put(2)
+q1.put(3)
+q1.put(5)
+q1.get()
+1
+q1.get()
+2
+q1.get()
+3
+q1.get()
+5
+q1.get()
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    q1.get()
+  File "<pyshell#21>", line 6, in get
+    raise EmptyQueueError("Очередь пустая")
+EmptyQueueError: EmptyQueueError: message: Очередь пустая
+>>> class EmptyQueueError(Exception):
+...     def __init__(self, message):
+...         self.message = message
+...     def __str__(self):
+...         return f"EmptyQueueError: message: {self.message}"
+... 
+...     
 >>> class Queue:
 ...     def __init__(self):
 ...         self.__queue = []
@@ -52,8 +91,8 @@ SyntaxError: unterminated string literal (detected at line 4)
 5
 >>> q1.get()
 Traceback (most recent call last):
-  File "<pyshell#31>", line 1, in <module>
+  File "<pyshell#45>", line 1, in <module>
     q1.get()
-  File "<pyshell#21>", line 6, in get
+  File "<pyshell#35>", line 6, in get
     raise EmptyQueueError("Очередь пустая")
 EmptyQueueError: EmptyQueueError: message: Очередь пустая
